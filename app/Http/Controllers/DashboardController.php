b53945638a080c10440d955bfd0d7b8b3b8e6d7f
Nadhif Lazuardi <berkasnadhif@gmail.com>
2024-10-09 15:50:11 +0700

    refactor: use only 1 API call in DashboardController

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -29,27 +29,11 @@ class DashboardController extends Controller
         if ($logResponse->successful()) {
             // Olah data yang didapat
             $logs = collect($logResponse->json('data')); // Ini mengubah response ke array
-
-            // Pisahkan data berdasarkan kode_kegiatan
-            $logPresensi = $logs->where('kode_kegiatan', 'P')->values(); // Kegiatan P
-            $logRapat = $logs->where('kode_kegiatan', 'R')->values(); // Kegiatan R
         } else {
             // Handle error, misalnya
             $logs = []; // Atau bisa juga return error message
         }
-
-        // Fetch data target rapat dari API
-        $rapatResponse = Http::get("http://e-office-undip.test/api/get-rapat-duration-target-by-user-id/{$userId}");
-
-        // Cek apakah request berhasil
-        if ($rapatResponse->successful()) {
-            // Olah data yang didapat
-            $rapatDurationTarget = collect($rapatResponse->json('data')); // Ini mengubah response ke array
-        } else {
-            // Handle error, misalnya
-            $rapatDurationTarget = []; // Atau bisa juga return error message
-        }
-
-        return view('dashboard', compact('title', 'logPresensi', 'logRapat', 'jumlahHariKerja', 'rapatDurationTarget'));
+        
+        return view('dashboard', compact('title', 'jumlahHariKerja', 'logs'));
     }
 }
